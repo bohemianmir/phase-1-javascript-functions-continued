1010816aa176845cc42d4448e2b158ff5eecb2c0
miraamer <mira.amr13@gmail.com>
2023-04-17 01:37:27 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}// code your solution here
+
+function mondayWork(activity = `go to the office`){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(special){
+    return function(word){
+        return `You are ${special}${word}${special}!`
+    }
+}
